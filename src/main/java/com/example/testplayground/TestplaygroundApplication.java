ec439db6afd01bc93252c5c43b100503551691cf
GoodA <good.arup@gmail.com>
2020-04-19 23:31:57 -0700

    init commit

---
+++ src/main/java/com/example/testplayground/TestplaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.testplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestplaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestplaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testplayground/TestplaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestplaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
